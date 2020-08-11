facbd2a6861675c2917996a3a8d9a5474f4410b8
u7172618 <ming.lei@anu.edu.au>
2020-08-10 22:22:46 -0700

    Complete Q2

---
+++ src/comp1110/labtest/Square.java
@@ -0,0 +1,11 @@
+package comp1110.labtest;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        System.out.println(i*i);
+    }
+}
